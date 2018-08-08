d98903a9c6deafe6a4c10ead78c2f6751ed5521c
Djair Carvalho <djaircarvalho.dj7@gmail.com>
2018-08-08 14:21:22 -0300

    adicionado tratamento de errros

--- app/routes/pdf_routes.js
+++ app/routes/pdf_routes.js
@@ -1,34 +1,58 @@
 const puppeteer = require('puppeteer');
 const fs = require('fs');
+
 function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0,
+            v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
 }
 module.exports = function(app) {
-  app.post('/pdf', (req, res) => {
-      (async () => {
-        const browser = await puppeteer.launch({
-          headless: true,
-          args: ['--no-sandbox', '--disable-setuid-sandbox'],
-      });
-        file_name = uuidv4() + '.pdf';
-        const page = await browser.newPage();
-        await page.goto(req.body.url, {waitUntil: 'networkidle2'});
-        await page.pdf({path: file_name, format: 'A4'});
-        await browser.close();
-        await fs.readFile(file_name, function(err, data) {
-          res.writeHead(200, {'Content-Type': 'application/pdf'});
-          res.write(data);
-          res.end();
-          try {
-            fs.unlinkSync(file_name);
-            console.log('successfully deleted ' +  file_name);
-          } catch (err) {
-            console.log('can\'t delete' + file_name);
-          }
-        });
-      })();
+    app.post('/pdf', (req, res, next) => {
+        (async() => {
+            const browser = await puppeteer.launch({
+                headless: true,
+                args: ['--no-sandbox', '--disable-setuid-sandbox'],
+            });
+            const page = await browser.newPage();
+
+            page.on('error', err => {
+                console.log('error happen at the page: ', err);
+            });
+
+            page.on('pageerror', pageerr => {
+                console.log('pageerror occurred: ', pageerr);
+            })
+
+            try {
+                file_name = uuidv4() + '.pdf'
+                await page.goto(req.body.url, {
+                    waitUntil: 'networkidle2'
+                });
+                await page.pdf({
+                    path: file_name,
+                    format: 'A4'
+                });
+                await browser.close();
+                await fs.readFile(file_name, function(err, data) {
+                    res.writeHead(200, {
+                        'Content-Type': 'application/pdf'
+                    });
+                    res.write(data);
+                    res.end();
+                    try {
+                        fs.unlinkSync(file_name);
+                        console.log('successfully deleted ' + file_name);
+                    } catch (err) {
+                        console.log('can\'t delete' + file_name);
+                        next(err)
+                    }
+                });
+            } catch (err) {
+                console.log('erro ', err);
+                next(err)
+            }
+        })();
     });
 };
